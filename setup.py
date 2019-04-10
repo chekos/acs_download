1c52f89980e1362140e3fd941b3aaf128f88b4a2
chekos <chekos.data@gmail.com>
2019-04-10 11:24:51 -0700

    up version

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/chekos/acs_download',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
 )
