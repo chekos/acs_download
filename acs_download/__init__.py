587682827aee96375a351b368838165a2a1f9675
chekos <chekos.data@gmail.com>
2019-04-10 12:53:49 -0700

    update version

--- acs_download/__init__.py
+++ acs_download/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Sergio Sánchez Zavala"""
 __email__ = 'sergio@cimarron.io'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 # -*- coding: utf-8 -*-
 
--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/chekos/acs_download',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
