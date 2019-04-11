b2c936ff7897e02232f1a28270a5210b3a946815
chekos <chekos.data@gmail.com>
2019-04-10 21:50:41 -0700

    Update cli.py

--- acs_download/cli.py
+++ acs_download/cli.py
@@ -10,7 +10,7 @@ import acs_download as acs
 @click.argument("--year", default = 2017, show_default=True, type=click.IntRange(min=2000, max=2017, clamp=True))
 @click.argument("--state", default = "Alaska", show_default=True, type=click.STRING)
 @click.option("--survey", default = "1-year", show_default=True, type=click.Choice(choices=("1-year", "5-year"), case_sensitive=False))
-@click.option("--person-or-household", default="person", type=click.Choice(choices=("person", "household), show_default=True)
+@click.option("--person-or-household", default="person", type=click.Choice(choices=("person", "household), show_default=True))
 @click.option("--download-path", default="../data/raw/", show_default=True, type=click.Path(exists=True, file_okay=False, writable=True))
 @click.option("--extract", default=True, show_default=True, type=click.Bool)
 @click.option("--extract-path", default="../data/interim/", show_default=True, type=click.Path(exists=True, file_okay=False, writable=True))
