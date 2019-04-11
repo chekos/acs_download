c4cb3e76201bd74f4138af794570d1fb9770eb2f
chekos <chekos.data@gmail.com>
2019-04-10 21:48:51 -0700

    first attempt at cli

--- acs_download/cli.py
+++ acs_download/cli.py
@@ -4,15 +4,22 @@
 import sys
 import click
 
+import acs_download as acs
 
 @click.command()
-def main(args=None):
+@click.argument("--year", default = 2017, show_default=True, type=click.IntRange(min=2000, max=2017, clamp=True)
+@click.argument("--state", default = "Alaska", show_default=True, type=click.STRING)
+@click.option("--survey", default = "1-year", show_default=True, type=click.Choice(choices=("1-year", "5-year", case_sensitive=False))
+@click.option("--person-or-household", default="person", type=click.Choice(choices=("person", "household), show_default=True)
+@click.option("--download-path", default="../data/raw/", show_default=True, type=click.Path(exists=True, file_okay=False, writable=True))
+@click.option("--extract", default=True, show_default=True, type=click.Bool)
+@click.option("--extract-path", default="../data/interim/", show_default=True, type=click.Path(exists=True, file_okay=False, writable=True))
+def main(year, state, survey, person_or_household, download_path, extract, extract_path):
     """Console script for acs_download."""
-    click.echo("Replace this message by putting your code into "
-               "acs_download.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
-    return 0
+    acs.get_data(year=year, state=state, survey=survey, person_or_household=poh, download_path=download_path, extract=extract, extract_path=extract_path)
 
+    return 0
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
+                                                                        
