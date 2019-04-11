bd5673622c8bbee7e5939c7034b9c0fd5d24dae3
chekos <chekos.data@gmail.com>
2019-04-10 22:16:57 -0700

    Update cli.py

--- acs_download/cli.py
+++ acs_download/cli.py
@@ -9,10 +9,10 @@ import acs_download as acs
 @click.command()
 @click.argument("--year", default = 2017, show_default=True, type=click.IntRange(min=2000, max=2017, clamp=True))
 @click.argument("--state", default = "Alaska", show_default=True, type=click.STRING)
-@click.option("--survey", default = "1-year", show_default=True, type=click.Choice(choices=("1-year", "5-year"), case_sensitive=False))
+@click.option("--survey", default = "1-year", type=click.Choice(choices=("1-year", "5-year"), case_sensitive=False))
 @click.option("--person-or-household", default="person", type=click.Choice(choices=("person", "household")), show_default=True)
-@click.option("--download-path", default="../data/raw/", show_default=True, type=click.Path(exists=True, file_okay=False, writable=True))
-@click.option("--extract", default=True, show_default=True, type=click.BOOL)
+@click.option("--download-path", default="../data/raw/", type=click.Path(exists=True, file_okay=False, writable=True))
+@click.option("--extract", default=True, type=click.BOOL)
 @click.option("--extract-path", default="../data/interim/", type=click.Path(exists=True, file_okay=False, writable=True))
 def main(year, state, survey, person_or_household, download_path, extract, extract_path):
     """Console script for acs_download."""
